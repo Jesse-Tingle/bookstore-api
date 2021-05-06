f340c02c53aa7d0ebc9b573d57d0eedf573a8175
Jesse-Tingle <jtingle0@gmail.com>
2021-05-05 22:10:36 -0400

    sets up basic get request to return meta data from spreadsheet

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-const server = require("./server.js")
+const express = require("express")
+const { google } = require("googleapis")
 
-const PORT = process.env.PORT || 6000
+const app = express()
 
-server.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}...`)
+app.get("/", async (req, res) => {
+	const auth = new google.auth.GoogleAuth({
+		keyFile: "credentials.json",
+		scopes: "https://www.googleapis.com/auth/spreadsheets",
+	})
+
+	const spreadsheetId = "14NCVbTgNgl5zVlYqThQGC-KRjeM-h2PA3m3ZPzP4su8"
+
+	//create client instance for auth
+	const client = await auth.getClient()
+
+	const googleSheets = google.sheets({
+		version: "v4",
+		auth: client,
+	})
+
+	const metaData = await googleSheets.spreadsheets.get({
+		auth,
+		spreadsheetId,
+	})
+
+	res.send(metaData.data)
+})
+
+app.listen(1337, (req, res) => {
+	console.log("Running on 1337")
 })
--- routes/router-index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router()
-
-// router.use("/auth", require("./auth/auth"));
-// router.use("/client", require("./client/client"));
-// router.use("/coach", require("./coach/coach.js"));
-// router.use("/media", require("./media/media-route"));
-
-module.exports = router
